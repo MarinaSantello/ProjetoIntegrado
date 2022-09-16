426aa46a4d4a274959b194fbf03da28d930f616f
Marina <98419843+MarinaSantello@users.noreply.github.com>
2022-09-16 10:07:23 -0300

    passando o projeto para o git

---
+++ BACK-END/modulos/cursos.js
@@ -0,0 +1,41 @@
+var cursos = [
+    {
+        "nome"  :   "Técnico em Desenvolvimento de Sistemas",
+        "sigla" :   "DS",
+        "icone" :   "https://image.shutterstock.com/image-vector/api-interface-vector-icon-600w-659203513.jpg",
+        "carga" :   "1200",
+    },
+    {
+        "nome"  :   "Técnico em Redes de Computadores",
+        "sigla" :   "RDC",
+        "icone" :   "https://img.icons8.com/ultraviolet/344/thin-client.png",
+        "carga" :   "1200"
+    }
+];
+
+const getCursos = (filtro) => {
+    const sigla = filtro.toUpperCase()
+    let xRetorno = null
+    let dadosCurso = []
+
+    cursos.forEach(element => {
+        if(element.sigla.includes(sigla)){
+            dadosCurso.push(
+                {
+                    nome : element.nome,
+                    sigla: element.sigla,
+                    icone: element.icone
+                }
+            )
+            xRetorno = dadosCurso
+        }
+    })
+
+    return xRetorno
+}
+
+module.exports = {
+    getCursos
+}
+
+console.log(getCursos('kjh'))
